5b5af293a54bcbefe39b80dab47d5d65c7b6b0e9
VAIBHAV PRATAP SINGH <vaibhav1bab@gmail.com>
2023-05-16 23:09:13 +0530

    Update process.java

--- process.java
+++ process.java
@@ -1,5 +1,24 @@
 import org.springframework.web.client.RestTemplate;
+Subject: Enthusiastic Software Engineer Seeking to Contribute to Dream11's Success
 
+Dear Dream11 Team,
+
+I hope this letter finds you well. I am writing to express my enthusiasm and passion for cricket, as well as my desire to join your organization as a Software Developer. I believe my skills and ideas can greatly contribute to the success of Dream11.
+
+Having worked as a software engineer at various companies, I have gained valuable experience in developing innovative solutions and tackling complex challenges. However, my true passion lies in cricket, and I am eager to combine my technical expertise with my love for the sport.
+
+While using the Dream11 platform, I have noticed a potential area for improvement that I would like to share. It seems that batsmen receive an undue advantage compared to bowlers. Even if a bowler performs exceptionally well, they are only awarded 10 points without taking a wicket. This creates an imbalance in the scoring system, where a struggling batsman can still gain an advantage over a bowler who has performed brilliantly.
+
+My suggestion is to introduce a scoring system where bowlers are awarded points for each over bowled in every format. This would ensure that bowlers receive a fair recognition for their efforts and contribution to the game. By implementing this idea, Dream11 can further enhance the platform's authenticity and engage a wider audience of cricket enthusiasts.
+
+I am confident that my technical skills, combined with my passion for cricket, can bring a fresh perspective to Dream11. I would love the opportunity to discuss my ideas in more detail and explore how I can contribute to the growth and success of your organization.
+
+Thank you for considering my application. I look forward to the possibility of joining the Dream11 team and making a significant impact in the world of fantasy cricket.
+
+Sincerely,
+
+[Your Name]
+[Contact Information]
 public class IncomeFlexDepletionReportProcessor implements ItemProcessor<Participant, PptReportDto> {
 
     private double depletionThreshold;
